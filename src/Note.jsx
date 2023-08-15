9696009d4b20780823b0f198bc90cdd20969b72c
Shivam <124341973+ShivamCo@users.noreply.github.com>
2023-08-15 11:43:14 +0530

    App Push

---
+++ src/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function Note(){
+    return <div className="note">
+        <h1 contentEditable="true">Note</h1>
+        <p contentEditable="true"> </p>
+    </div>
+}
+
+export default Note
